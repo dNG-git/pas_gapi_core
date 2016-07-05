f3f59670e24dbb245b8f08010c3c4dd58f9fcf74
NotTheEvilOne <git-NTEO@vplace.de>
2016-07-05 18:37:19 +0200

    Enhancement: Added "dNG.pas.gapi.CallbackContextMixin" for handling gapi mainloop callbacks and signal based thread synchronization

---
+++ src/dNG/pas/gapi/callback_context_mixin.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+direct PAS
+Python Application Services
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+https://www.direct-netware.de/redirect?pas;gapi;core
+
+The following license agreement remains valid unless any additions or
+changes are being made by direct Netware Group in a written form.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+----------------------------------------------------------------------------
+https://www.direct-netware.de/redirect?licenses;gpl
+----------------------------------------------------------------------------
+#echo(pasGapiCoreVersion)#
+#echo(__FILEPATH__)#
+"""
+
+from threading import Event
+
+class CallbackContextMixin(object):
+#
+	"""
+"CallbackContextMixin" provides a context manager for handling gapi mainloop
+callbacks and signal based thread synchronization.
+
+:author:     direct Netware Group
+:copyright:  (C) direct Netware Group - All rights reserved
+:package:    pas.gapi
+:subpackage: core
+:since:      v0.1.04
+:license:    https://www.direct-netware.de/redirect?licenses;gpl
+             GNU General Public License 2
+	"""
+
+	def __init__(self):
+	#
+		"""
+Constructor __init__(CallbackContextMixin)
+
+:since: v0.1.04
+		"""
+
+		self._callback_event = None
+		"""
+Callback event used for signal based thread synchronization
+		"""
+		self._callback_result = None
+		"""
+Callback result
+		"""
+	#
+
+	def __enter__(self):
+	#
+		"""
+python.org: Enter the runtime context related to this object.
+
+:since: v0.1.04
+		"""
+
+		self._callback_event = Event()
+		self._callback_result = None
+
+		return self
+	#
+
+	def __exit__(self, exc_type, exc_value, traceback):
+	#
+		"""
+python.org: Exit the runtime context related to this object.
+
+:return: (bool) True to suppress exceptions
+:since:  v0.1.04
+		"""
+
+		if (self._callback_event is None): self._callback_event.set()
+		self._callback_result = None
+
+		return False
+	#
+#
+
+##j## EOF
